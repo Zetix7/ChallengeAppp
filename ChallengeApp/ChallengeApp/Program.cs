441aba0372d51d65a78b8e5cc58df29bb03b9af7
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-11-21 17:23:07 +0100

    Example of variables

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int myAge = 30;
+int newAge = myAge + 1;
+int myInt = int.MaxValue;
+uint myUint = uint.MaxValue;
+long myLong = long.MaxValue;
+ulong myUlong = ulong.MaxValue;
+
+float myFloat = float.MaxValue;
+double myDouble = double.MaxValue;
+decimal myDecimal = decimal.MaxValue;
+
+string firstName = "Greg";
+string lastName = "Zet";
+string result = firstName + lastName + myAge;
+Console.WriteLine(result);
+char myChar = 'm';
+var result2 = firstName.ToArray();
+
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
