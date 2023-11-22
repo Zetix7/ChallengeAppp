c02861347602620cefd636c509b9ff417da64967
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-11-22 13:40:36 +0100

    Counting digits in number and show stats

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,15 +1,24 @@
-﻿List<string> daysOfWeek = new List<string>
-{
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
-};
+﻿long number = 98010245678;
 
-foreach(var day in daysOfWeek)
+string numberInString = number.ToString();
+int[] digitsInNumber = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+foreach (var digit in numberInString)
 {
-    Console.WriteLine(day);
+    if (digit == '0') { digitsInNumber[0]++; }
+    else if (digit == '1') { digitsInNumber[1]++; }
+    else if (digit == '2') { digitsInNumber[2]++; }
+    else if (digit == '3') { digitsInNumber[3]++; }
+    else if (digit == '4') { digitsInNumber[4]++; }
+    else if (digit == '5') { digitsInNumber[5]++; }
+    else if (digit == '6') { digitsInNumber[6]++; }
+    else if (digit == '7') { digitsInNumber[7]++; }
+    else if (digit == '8') { digitsInNumber[8]++; }
+    else if (digit == '9') { digitsInNumber[9]++; }
+}
+
+Console.WriteLine("Result for count: " + number);
+
+for (var i = 0; i < digitsInNumber.Length; i++)
+{
+    Console.WriteLine(i + " => " + digitsInNumber[i]);
 }
