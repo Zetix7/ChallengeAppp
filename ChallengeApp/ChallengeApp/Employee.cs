a507611fc46add244a0f4e10f8741e8f65f21539
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-11-22 15:28:35 +0100

    Create class Employee, field, properties and method, Create 3 objects of Employee adding random grades (1-10) and counting them, Show the highest Result of employee on the screen

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,29 @@
+﻿namespace ChallengeApp;
+
+public class Employee
+{
+    private int _age;
+    private List<int> _grades = new();
+
+    public Employee(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        _age = age;
+    }
+    public string FirstName { get; }
+    public string LastName { get; }
+
+    public int Result
+    {
+        get
+        {
+            return _grades.Sum();
+        }
+    }
+
+    public void AddGrade(int grade)
+    {
+        _grades.Add(grade);
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,10 +1,33 @@
 ﻿using ChallengeApp;
 
-var user1 = new User("Greg", "passwordGreg");
-var user2 = new User("Liz", "passwordLiz");
-var user3 = new User("Natalie", "passwordNatalie");
+var employee1 = new Employee("Greg", "Zet", 30);
+var employee2 = new Employee("Liz", "Olsen", 35);
+var employee3 = new Employee("Natalie", "Portman", 40);
 
-user1.AddScore(2);
-user1.AddScore(5);
-var result = user1.Result;
-Console.WriteLine(result);
+List<Employee> employees = new List<Employee>
+{
+    employee1,
+    employee2,
+    employee3
+};
+
+foreach (var employee in employees)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        employee.AddGrade(new Random().Next(1, 10));
+    }
+}
+
+var maxGrades = 0;
+var employeeWinner = employee1;
+foreach (var employee in employees)
+{
+    if (maxGrades < employee.Result)
+    {
+        maxGrades = employee.Result;
+        employeeWinner = employee;
+    }
+}
+
+Console.WriteLine(employeeWinner.FirstName + " " + employeeWinner.LastName + ", the highest result is: " + employeeWinner.Result);
