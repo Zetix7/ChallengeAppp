d3466cd7042c50d9d6a102ef87b01f318d18d42e
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-11-22 13:03:41 +0100

    Better view of List

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,11 +1,13 @@
-﻿List<string> daysOfWeek = new List<string>();
-daysOfWeek.Add("Monday");
-daysOfWeek.Add("Tuesday");
-daysOfWeek.Add("Wednesday");
-daysOfWeek.Add("Thursday");
-daysOfWeek.Add("Friday");
-daysOfWeek.Add("Saturday");
-daysOfWeek.Add("Sunday");
+﻿List<string> daysOfWeek = new List<string>
+{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
 
 foreach(var day in daysOfWeek)
 {
