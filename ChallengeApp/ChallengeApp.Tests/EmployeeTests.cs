13002e8d7c8247b299ffd2977c95d9a9a6442023
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-11-27 13:58:58 +0100

    New Employee tests, fix code

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -3,27 +3,64 @@
 public class EmployeeTests
 {
     [Test]
-    public void Add3PointsAndCheckCorrectStatistics()
+    public void Add3PointsAndReturnCorrectMin()
+    {
+        var employee = new Employee("Greg", "Zet");
+        employee.AddGrade(35);
+        employee.AddGrade('e');
+        employee.AddGrade(63);
+
+        var statistics = employee.GetStatistics();
+
+        Assert.AreEqual(20, statistics.Min);
+    }
+
+    [Test]
+    public void Add3PointsAndReturnCorrectMax()
+    {
+        var employee = new Employee("Greg", "Zet");
+        employee.AddGrade('B');
+        employee.AddGrade(17.77);
+        employee.AddGrade(97);
+
+        var statistics = employee.GetStatistics();
+
+        Assert.AreEqual(97, statistics.Max);
+    }
+
+    [Test]
+    public void Add3PointsAndReturnCorrectAverage()
     {
         var employee = new Employee("Greg", "Zet");
         employee.AddGrade(4);
-        employee.AddGrade(7);
         employee.AddGrade(6);
-        
-        employee.GetStatistics(out Statistics statistics);
+        employee.AddGrade(7);
 
-        Assert.AreEqual(7, statistics.Max);
-        Assert.AreEqual(4, statistics.Min);
+        var statistics = employee.GetStatistics();
+
         Assert.AreEqual(Math.Round(5.67, 2), Math.Round(statistics.Average, 2));
+    }
+
+    [Test]
+    public void Add3PointsAndReturnCorrectAverageLetter()
+    {
+        var employee = new Employee("Greg", "Zet");
+        employee.AddGrade(42);
+        employee.AddGrade(67);
+        employee.AddGrade(77);
+
+        var statistics = employee.GetStatistics();
+
+        Assert.AreEqual('B', statistics.AverageLetter);
     }
 
     [Test]
     public void Check2DifferentEmployees()
     {
         var employee1 = new Employee("Chris", "Evans");
-        
+
         var employee2 = GetEmployee(employee1);
-        
+
         Assert.AreNotEqual(employee2, employee1);
     }
 
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,24 +2,20 @@
 
 Console.WriteLine("Welcome in XYZ program to rate employees");
 Console.WriteLine();
-Console.WriteLine("Insert grade of employee: ");
+
 var employee = new Employee("Greg", "Zet");
 
-var input = Console.ReadLine();
-employee.AddGrade(input);
-
-while (true)
+do
 {
     Console.WriteLine("Insert next grade of employee: ");
-    input = Console.ReadLine();
+    var input = Console.ReadLine();
 
-    if(input == "q" || input == "Q")
+    if (input == "q" || input == "Q")
     {
         break;
     }
     employee.AddGrade(input);
-}
-
+} while (true);
 
 var statistics = employee.GetStatistics();
 
