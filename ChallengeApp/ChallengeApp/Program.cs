908b59a3f51c165b9d27cf746197bd36a4687722
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-11-27 13:36:43 +0100

    Added interaction with user

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,15 +1,29 @@
 ﻿using ChallengeApp;
 
+Console.WriteLine("Welcome in XYZ program to rate employees");
+Console.WriteLine();
+Console.WriteLine("Insert grade of employee: ");
 var employee = new Employee("Greg", "Zet");
-employee.AddGrade(7);
-employee.AddGrade(6.456789987);
-employee.AddGrade(1.986);
-employee.AddGrade("4,1237");
-employee.AddGrade("7,45677994566");
-employee.AddGrade("687");
-employee.AddGrade("test");
-employee.AddGrade('e');
+
+var input = Console.ReadLine();
+employee.AddGrade(input);
+
+while (true)
+{
+    Console.WriteLine("Insert next grade of employee: ");
+    input = Console.ReadLine();
+
+    if(input == "q" || input == "Q")
+    {
+        break;
+    }
+    employee.AddGrade(input);
+}
+
+
 var statistics = employee.GetStatistics();
+
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
 Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"AverageLetter: {statistics.AverageLetter}");
