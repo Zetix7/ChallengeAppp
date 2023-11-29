0c5ff1b4473dec5e155a938742e6ee61b1205c1f
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2023-11-29 13:25:44 +0100

    Implementation of EmployeeInMemory class

--- ChallengeApp/ChallengeApp/EmployeeInMemory.cs
+++ ChallengeApp/ChallengeApp/EmployeeInMemory.cs
@@ -2,37 +2,120 @@
 
 public class EmployeeInMemory : EmployeeBase
 {
+    private readonly List<float> _grades = new();
+
     public EmployeeInMemory(string firstName, string lastName) : base(firstName, lastName)
     {
     }
 
     public override void AddGrade(float grade)
     {
-        throw new NotImplementedException();
+        if (grade >= 0 && grade <= 100)
+        {
+            _grades.Add(grade);
+        }
+        else
+        {
+            throw new Exception($"ERROR: Invalid value '{grade}'! Float must be from 0 to 100.");
+        }
     }
 
     public override void AddGrade(string grade)
     {
-        throw new NotImplementedException();
-    }
-
-    public override void AddGrade(double grade)
-    {
-        throw new NotImplementedException();
-    }
-
-    public override void AddGrade(char grade)
-    {
-        throw new NotImplementedException();
+        if (int.TryParse(grade, out int intGrade))
+        {
+            AddGrade(intGrade);
+        }
+        else if (double.TryParse(grade, out double doubleResult))
+        {
+            AddGrade(doubleResult);
+        }
+        else if (float.TryParse(grade, out float floatResult))
+        {
+            AddGrade(floatResult);
+        }
+        else if (grade.Length == 1)
+        {
+            AddGrade(grade[0]);
+        }
+        else
+        {
+            throw new Exception($"ERROR: Invalid value '{grade}'! String is not float.");
+        }
     }
 
     public override void AddGrade(int grade)
     {
-        throw new NotImplementedException();
+        var floatGrade = grade;
+        AddGrade(floatGrade);
+    }
+
+    public override void AddGrade(double grade)
+    {
+        var floatValue = (float)grade;
+        AddGrade(floatValue);
+    }
+
+    public override void AddGrade(char grade)
+    {
+        switch (grade)
+        {
+            case 'a' or 'A':
+                _grades.Add(100);
+                break;
+            case 'b' or 'B':
+                _grades.Add(80);
+                break;
+            case 'c' or 'C':
+                _grades.Add(60);
+                break;
+            case 'd' or 'D':
+                _grades.Add(40);
+                break;
+            case 'e' or 'E':
+                _grades.Add(20);
+                break;
+            default:
+                throw new Exception("ERROR: Wrong grade! Input grade from 'a' to 'e'.");
+        }
     }
 
     public override Statistics GetStatistics()
     {
-        throw new NotImplementedException();
+        var statistics = new Statistics();
+        statistics.Average = 0;
+        statistics.Max = 0;
+        statistics.Min = 100;
+        statistics.AverageLetter = 'X';
+
+        foreach (var grade in _grades)
+        {
+            statistics.Max = Math.Max(statistics.Max, grade);
+            statistics.Min = Math.Min(statistics.Min, grade);
+            statistics.Average += grade;
+        }
+
+        statistics.Average /= _grades.Count;
+
+        switch (statistics.Average)
+        {
+            case var average when average >= 80:
+                statistics.AverageLetter = 'A';
+                break;
+            case var average when average >= 60:
+                statistics.AverageLetter = 'B';
+                break;
+            case var average when average >= 40:
+                statistics.AverageLetter = 'C';
+                break;
+            case var average when average >= 20:
+                statistics.AverageLetter = 'D';
+                break;
+            default:
+                statistics.AverageLetter = 'E';
+                break;
+        }
+
+        return statistics;
     }
 }
